fa7fa5008d51e022766bbfbbe71c39510e14f683
FeryKevin <kevin.fery@outlook.com>
2023-10-28 18:57:22 +0200

    add score

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,12 @@ button.classList.add("button", "play-button");
 const buttonInterface = document.getElementById("interface");
 buttonInterface.appendChild(button);
 
+// Score
+let score = document.getElementById("score");
+
+// Play
 button.addEventListener("click", (e) => {
   e.target.remove();
   alert("JOUE");
+  score.innerHTML = "Score : 01";
 });
