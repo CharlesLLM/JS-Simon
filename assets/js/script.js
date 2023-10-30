4af05b7307247a1e0b0224fba83e00abcf8faea3
FeryKevin <kevin.fery@outlook.com>
2023-10-30 22:18:30 +0100

    v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,40 @@ scoreDiv.classList.add("score");
 let countScore = 0;
 scoreDiv.innerHTML = "Score: " + countScore;
 
+
+
+const cases = document.querySelectorAll('.case');
+
+
+// function test(){
+//   this.className += ' active';
+// }
+
+// setTimeout("test()", 2000);
+
+cases.forEach(cas => {
+  cas.addEventListener('click', function() {
+    this.className += ' active';
+  });
+});
+
+
+
+
+
+
+// document.getElementsByClassName("case").addEventListener("click", function() {
+//   alert("Hello World!");
+// });
+
+
+
+
+
+
+
+
+
 // Play
 playButton.addEventListener("click", (e) => {
   e.target.remove();
